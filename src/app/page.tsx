93245bffa3614202bd2725aa1f8aaefa63a88eaa
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-09-04 10:03:09 -0300

    fix: tirando barra de rolagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <section className='min-h-screen flex items-center'>
+        <section className='min-h-[calc(100vh-3.5rem)] flex items-center'>
           <Hero />
         </section>
       </main>
